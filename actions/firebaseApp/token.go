77e4b48224b701f2a05ce2c60ac18f32b3a51510
James Kopatsis <jkopatsis@gmail.com>
2024-08-28 08:36:35 -0700

    tb merged push

--- actions/firebaseApp/token.go
+++ actions/firebaseApp/token.go
@@ -3,23 +3,17 @@ package firebaseApp
 import (
 	"context"
 	"errors"
-	"strings"
 
 	"github.com/gobuffalo/buffalo"
 )
 
 func VerifyTokenAndReturnUID(c buffalo.Context) (string, error) {
-	authHeader := c.Request().Header.Get("Authorization")
-	if authHeader == "" {
-		return "", errors.New("missing authorization header")
+	idToken := c.Request().FormValue("idToken")
+	if idToken == "" {
+		return "", errors.New("missing idToken in form data")
 	}
 
-	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-	if tokenString == authHeader {
-		return "", errors.New("invalid token format")
-	}
-
-	token, err := FirebaseAuth.VerifyIDToken(context.Background(), tokenString)
+	token, err := FirebaseAuth.VerifyIDToken(context.Background(), idToken)
 	if err != nil {
 		return "", errors.New("failed to verify token")
 	}
